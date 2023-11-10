bad501a9e4420e1334b4eeab483f7653bd649271
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-10 11:22:51 +0100

    fix: parole con lunghezza uguale

--- snack2/main.js
+++ snack2/main.js
@@ -1,10 +1,12 @@
 'use strict';
 
 // L’utente inserisce due parole in successione, con due prompt.
-// Il software stampa prima la parola più corta, poi la parola più lunga.
+
 
 const parola1 = prompt('inserisci la prima parola');
 const parola2 = prompt('inserisci la seconda parola');
+
+// Il software stampa prima la parola più corta, poi la parola più lunga.
 
 if (parola1.length < parola2.length) {
     console.log(parola2);
@@ -12,4 +14,7 @@ if (parola1.length < parola2.length) {
 } else if (parola2.length < parola1.length) {
     console.log(parola1);
     console.log(parola2);  
+} else {
+    console.log(parola1, parola2);
 }
+
