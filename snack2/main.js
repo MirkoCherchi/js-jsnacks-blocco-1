6bd2114b5e32c4477410043839b05ae4930ca0ce
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-10 11:29:43 +0100

    fix: ordine lunghezza parole

--- snack2/main.js
+++ snack2/main.js
@@ -9,11 +9,11 @@ const parola2 = prompt('inserisci la seconda parola');
 // Il software stampa prima la parola più corta, poi la parola più lunga.
 
 if (parola1.length < parola2.length) {
-    console.log(parola2);
-    console.log(parola1);   
-} else if (parola2.length < parola1.length) {
     console.log(parola1);
-    console.log(parola2);  
+    console.log(parola2);   
+} else if (parola2.length < parola1.length) {
+    console.log(parola2);
+    console.log(parola1);  
 } else {
     console.log(parola1, parola2);
 }
