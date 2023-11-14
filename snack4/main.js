7cca80914ef9c37348befe3027cd0e3961df7ba6
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-14 10:46:33 +0100

    feat: snack4

---
+++ snack4/main.js
@@ -0,0 +1,29 @@
+'use strict'
+
+// Crea un array vuoto e chiedi all’utente un numero da inserire nell’array. 
+// Continua a chiedere i numeri all’utente e a inserirli nell’array fino a quando la somma degli elementi è inferiore di 50.
+const numero = [];
+let sommaN = 0;
+
+while (sommaN < 50) {
+    const numeroUtente = prompt('inserisci numero')
+    if (isNaN(numeroUtente)) {
+        console.log('numero non valido');
+    } else {
+        numero.push(numeroUtente);
+        sommaN += +numeroUtente;
+    }
+    
+}
+
+console.log(numero);
+console.log(sommaN);
+
+
+
+
+
+
+
+
+
